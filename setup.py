63902222fabb695010f9c31668b6782055af8cf2
Kevin Connor <kevinpaulconnor@gmail.com>
2018-09-19 15:48:41 -0500

    well, it needs to be a python package if we're going to install it like one

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rmi-qb-python',
+      version='0.1.1',
+      description='Reading & Math Inc Python SDK for Quick Base',
+      url='https://github.com/ReadingandMath/Quickbase-Python-SDK',
+      author='Kevin Connor',
+      author_email='kevin.connor@servetogrow.org',
+      license='MIT',
+      packages=['rmi-qb-python'],
+      zip_safe=False)
