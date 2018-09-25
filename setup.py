6ea9bd5bacd27e2c703f171c41a72d79902f5858
Kevin Connor <kevinpaulconnor@gmail.com>
2018-09-25 09:36:51 -0500

    add packaging tools, version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='rmi_qb_python',
-      version='0.1.2',
+      version='0.1.3',
       description='Reading & Math Inc Python SDK for Quick Base',
       long_description=long_description,
       long_description_content_type="text/markdown",
